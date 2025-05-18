6a8e5d2bbcf3b611b4b0f986208c8af88469763e
Allen Victor <145675987+SassukeKun@users.noreply.github.com>
2025-05-19 00:03:31 +0200

    Fix undefined isAuthPage variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,21 +2,32 @@ import '../../styles/globals.css'
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 import ClientLayout from '@/components/ClientLayout';
+import { usePathname } from 'next/navigation';
+import Sidebar from '@/components/Sidebar';
+import PlayerBar from '@/components/PlayerBar';
+import Providers from '@/utils/providers';
 
 const inter = Inter({ subsets: ["latin"] });
 
-// Definição dos metadados do site (lado do servidor)
 export const metadata: Metadata = {
   title: "EiMusic - Plataforma de streaming",
   description: "Plataforma de streaming de musicas nacionais",
 };
 
-// Componente principal de layout (lado do servidor)
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const isAuthPage = [
+    '/login',
+    '/register',
+    '/artist/login',
+    '/artist/register',
+    '/auth/callback'
+  ].includes(pathname);
+
   return (
     <html lang="pt-PT">
       <head />
